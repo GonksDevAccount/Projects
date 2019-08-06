2be28d8541a275a3a12ee9b86d335258dd6484c3
GonksDevAccount <53813822+GonksDevAccount@users.noreply.github.com>
2019-08-06 05:05:28 -0400

    Add files via upload

---
+++ CameraMove.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public float panSpeed = 10f;
+    public float panBoarderThickness = 10f;
+    public Vector2 panLimit;
+    public float scrollSpeed = 20f;
+
+
+    void Update()
+    {
+
+        Vector3 pos = transform.position;
+
+        if(Input.GetKey("a") || Input.mousePosition.x <= panBoarderThickness)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBoarderThickness)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBoarderThickness)
+        {
+            pos.y -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBoarderThickness)
+        {
+            pos.y += panSpeed * Time.deltaTime;
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.z += scroll * scrollSpeed * Time.deltaTime;
+
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.y = Mathf.Clamp(pos.y, -panLimit.y, panLimit.y);
+
+        transform.position = pos;
+
+    }
+
+
+}
